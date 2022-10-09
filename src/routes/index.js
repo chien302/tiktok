300093fc618f813acb5164731969568c1a53f837
chien vuong <chienvuong302@gmail.com>
2022-10-09 16:05:55 +0700

    routes/layout

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+
+import HeaderOnly from '~/components/Layout/HeaderOnly';
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/profile', component: Profile },
+    { path: '/search', component: Search, layout: null },
+];
+const privateROutes = [];
+
+export { publicRoutes, privateROutes };
